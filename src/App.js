511823265bfe4e2e0664bbd15dec8b5a4c2959ab
Raymond Santos <ragustinesantos@gmail.com>
2024-01-30 17:14:14 -0700

    Update app.js -- HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import {HashRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import "./App.css"
 import WeatherApp from './pages/Weather';
 import Currency from './pages/Currency';
 
 function App() {
   return (
-    <HashRouter base="/">
       <Routes>
         <Route path="/" element={<WeatherApp />}/>
         <Route path="/Currency" element={<Currency />}/>
       </Routes>
-    </HashRouter>
   );
 }
 
