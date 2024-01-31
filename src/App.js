41c33950a3bd7bbbc5a9877a869bd7bb20eafe12
Raymond Santos <ragustinesantos@gmail.com>
2024-01-30 18:25:28 -0700

    Reverted to previous working version; moving to vercel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "./App.css"
 import WeatherApp from './pages/Weather';
 import Currency from './pages/Currency';
 
 function App() {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<WeatherApp />}/>
         <Route path="/Currency" element={<Currency />}/>
       </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -181,7 +181,7 @@ export default function WeatherApp() {
                     <div class="navbar-nav">
                         <a class="nav-link" aria-current="page" href="/">Home</a>
                         <Link class="nav-link" to="/">Weather</Link>
-                        <Link class="nav-link" to="/Currency">Currency</Link>
+                        <a class="nav-link" href='#'>Currency</a>
                     </div>
                 </div>
             </div>
